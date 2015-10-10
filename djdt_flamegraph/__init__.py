ef31e7d72e86cf88caa46a14d924a98ccd75d36f
Bo Lopker <blopker@23andme.com>
2015-10-10 16:52:28 -0700

    Export panel in root

---
+++ djdt_flamegraph/__init__.py
@@ -0,0 +1,1 @@
+from .djdt_flamegraph import FlamegraphPanel
--- tests/test_djdt_flamegraph.py
+++ tests/test_djdt_flamegraph.py
@@ -8,6 +8,7 @@ Tests for `djdt_flamegraph` module.
 import unittest
 
 from djdt_flamegraph import djdt_flamegraph
+from djdt_flamegraph import FlamegraphPanel
 
 
 class TestDjdtFlamegraph(unittest.TestCase):
